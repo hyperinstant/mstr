24c13fc482d0b2220edba94fb02a56cce65e3172
Yevhenii Kurtov <yevhenii.kurtov@dyad.net>
2025-01-17 22:23:08 +0000

    Add token autorefreshment functionality

--- lib/sspotify/token_manager.ex
+++ lib/sspotify/token_manager.ex
@@ -1,13 +1,21 @@
 defmodule SSpotify.TokenManager do
   @ets_table :spotify_token_store
+  # safety buffer in seconds
+  @refresh_buffer 60
+
   alias SSpotify.TokenManager.State
   use GenServer
   require Logger
 
-  # Server
+  ## Client
+  def token() do
+    Keyword.fetch!(:ets.lookup(@ets_table, :token), :token)
+  end
+
+  ## Server
 
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args)
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
   def init(args) do
@@ -19,9 +27,14 @@ defmodule SSpotify.TokenManager do
     {:noreply, refresh_token(state)}
   end
 
+  def handle_info(:refresh_token, state) do
+    {:noreply, refresh_token(state)}
+  end
+
   defp refresh_token(state) do
     state = request_token!(state)
-    :ets.insert(:spotify_token_store, {:token, state.token})
+    :ets.insert(@ets_table, {:token, state.token})
+    schedule_refresh(state.expires_in)
     state
   end
 
@@ -47,7 +60,9 @@ defmodule SSpotify.TokenManager do
     end
   end
 
-  def token() do
-    Keyword.fetch!(:ets.lookup(:spotify_token_store, :token), :token)
+  defp schedule_refresh(expires_in) do
+    # Schedule refresh before token expires (subtracting buffer time)
+    refresh_in = max((expires_in - @refresh_buffer) * 1000, 0)
+    Process.send_after(self(), :refresh_token, refresh_in)
   end
 end
